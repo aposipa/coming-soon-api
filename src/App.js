37e11bf50e636d3bfc146b99e83260120c2296ca
aposipa <andrew.posipanka@gmail.com>
2021-08-29 16:19:21 -0400

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import MovieCard from "./components/MovieCards";
+import Scrapper from "./components/Scrapper";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = () => {
+    const options = {
+      method: "GET",
+      url: "https://imdb8.p.rapidapi.com/title/get-coming-soon-movies",
+      params: {
+        homeCountry: "US",
+        purchaseCountry: "US",
+        currentCountry: "US",
+      },
+      headers: {
+        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+        "x-rapidapi-key": "7577630e23msh5e3f19cb7363e31p1f316cjsndcd0b76b24e5",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const fullMovielink = response.data.map(
+          (movie) => `http://imdb.com${movie}`
+        );
+        setMovies(fullMovielink);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  console.log(movies);
+  useEffect(() => {
+    getMovies();
+    // const fullMovieLink = movies.map((movie) => `http://imdb.com${movie}`);
+    // setMovies(fullMovieLink);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MovieCard movies={movies} />
+      <Scrapper movies={movies} />
+    </>
   );
 }
 
 export default App;
+
+// .forEach((element) => {
+//   console.log(`http://imdb.com/${element}`);
+// });
---
+++ src/components/MovieCards.js
@@ -0,0 +1,16 @@
+function MovieCard({ movies }) {
+  return (
+    <div>
+      <h1>this better work?</h1>
+      {/* <div className="movie-list">
+        {movies.map((movie) => (
+          <div className="movie-card">
+            <li>{movie}</li>
+          </div>
+        ))}
+      </div> */}
+    </div>
+  );
+}
+
+export default MovieCard;
